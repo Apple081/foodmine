549cf51d1829f7b9faabf744fad951d33a7a697b
Apple081 <150267555+Apple081@users.noreply.github.com>
2023-11-08 20:48:30 +0530

    Create Angular App

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -25,7 +25,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'public', 'index.html'))
 })
 
-const port = process.env.PORT || 5007;
+const port = process.env.PORT || 5507;
 app.listen(port, () => {
     console.log("Website served on http://localhost:" + port);
 })
